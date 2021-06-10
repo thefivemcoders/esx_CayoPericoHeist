110ccd6787477128d215c774d05951a60ff2258f
TheFiveMCoders <44032346+thefivemcoders@users.noreply.github.com>
2021-06-10 16:37:42 +0530

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,16 @@
+Locales['en'] = {
+  ['shop_robbery'] = 'Cayo Perico Mansion Heist',
+  ['press_to_rob'] = 'press ~INPUT_CONTEXT~ to %s',
+  ['robbery_timer'] = 'CayoPerico Mansion Heist : %s seconds remaning to Finish',
+  ['recently_robbed'] = 'This Mansion was recently been robbed. Please wait %s seconds and try again.',
+  ['rob_in_prog'] = 'Alert: ALL AVAILABLE OFFICERS RESPOND IMMEDIATELY , PLEASE RUSH TO Cayo Perico Island',
+  ['started_to_rob'] = 'You started %s',
+  ['alarm_triggered'] = 'The alarm has been triggered',
+  ['robbery_complete'] = 'The Heist has been successful, you stole $%s',
+  ['robbery_complete_at'] = 'Heist successful at %s',
+  ['robbery_cancelled'] = 'the heist has been cancelled!',
+  ['robbery_cancelled_at'] = 'The heist at %s has been cancelled!',
+  ['min_police'] = 'there must be at least %s cops in town to rob.',
+  ['robbery_already'] = 'A heist is already in progress.',
+  ['no_threat'] = 'You do not pose a threat.',
+}
